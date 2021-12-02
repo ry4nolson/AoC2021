56b60a92f5e42969df904c2855d61391627ba7ab
Ryan Olson <ryan.olson@fsgi.com>
2021-12-02 10:06:04 -0600

    day 2

---
+++ 2.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+//load 1.txt
+const filePath = path.join(__dirname, '2.txt');
+const fileContent = fs.readFileSync(filePath, 'utf-8');
+
+let horizontal = 0;
+let depth = 0;
+
+//split fileContent into array of lines
+const lines = fileContent.split('\n');
+//parse each line into array of numbers
+const movements = lines.map(line => {
+  const parts = line.split(' ');
+  return {
+    direction: parts[0],
+    distance: parseInt(parts[1])
+  }
+});
+
+//loop through each line, stargin with the second line
+for (let i = 0; i < movements.length; i++) {
+  const movement = movements[i];
+  if (movement.direction === 'forward') {
+    horizontal += movement.distance;
+  } else if (movement.direction === 'down') {
+    depth += movement.distance;
+  } else if (movement.direction === 'up') {
+    depth -= movement.distance;
+  }
+}
+
+console.log(horizontal * depth);
+
